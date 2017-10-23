7b240a61e51bd73addd0da709eab379480f19e64
luisaGonzales <luisagc711@gmail.com>
2017-10-23 15:01:47 -0500

    Avance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {connect} from 'redux-zero/react';
 // import {} from 'Actions.js';
+import plane from "./img/plane.svg";
+import {Grid, Row, Col} from 'react-bootstrap';
+
+const Head = ({image}) => {
+  return (
+    <Row className='text-center'>
+      <Col mdOffset={3}  md={6} mdOffset={3}>
+        <img src={image} alt="imagen"/>
+      </Col>
+    </Row>
+  );
+}
+
+const Quiz = ({questionActual}) => {
+  return (
+    <Row className=''>
+
+    </Row>
+  );
+}
+
+const Progress = ({totalAnswers, totalQuestions}) => {
+  <Row className="estado" id="progreso">
+    <Col md={12} xs={12}>
+      <div>
+        {totalAnswers} de {totalQuestions} preguntas contestadas
+      </div>
+      <div className="progress">
+        <div className="progress-bar" role="progressbar" aria-valuemax="100" style={{ width: {totalAnswers} * 20 + '%', height: '10px' }}></div>                                 
+      </div>
+    </Col>
+  </Row>
+
+}
+
+const Socials = () => {
+  return ( 
+    <Row>
+      <Col mdOffset={3} md={6} id='redesSociales'>
+        <span className="fa-stack fa-lg ">
+                <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x twitter"></i>
+                <i className="fa fa-twitter fa-stack-1x"></i>
+          </span>
+          <span className="fa-stack fa-lg ">
+                <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x facebook"></i>
+                <i className="fa fa-facebook fa-stack-1x"></i>
+          </span>
+          <span className="fa-stack fa-lg ">
+                <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x google"></i>
+                <i className="fa fa-google-plus fa-stack-1x"></i>
+          </span>
+      </Col>
+    </Row>
+  );
+}
+
+
+
+
+
 
 const App = ({questions, selected}) => {
   return (
-      <div>
-        <h1>Funciona</h1>
+    <Grid className='quiz text-center'>
+      <Head image={questions[0].img}/>
+      <div className="contenido">
+        <Socials />
       </div>
+    </Grid>
   );
 }
 
--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,6 @@
 import createStore from 'redux-zero';
 
-let ALLQUESTIONS = [
+let AllQuestions = [
     {
         id: 1,
         question: `¿Cuál es la aerolínea más antigua del mundo?`,
@@ -8,7 +8,7 @@ let ALLQUESTIONS = [
             "Avianca", "KLM", "Qantas"
         ],
         correct: "KLM",
-        img: "img/plane.svg"
+        img: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/plane.svg"
     }, {
         id: 2,
         question: `¿Cuál es el puerto más grande del mundo?`,
@@ -16,7 +16,7 @@ let ALLQUESTIONS = [
             "Puerto de Singapur", "Puerto de Rotterdam", "Puerto de Shangai"
         ],
         correct: "Puerto de Shangai",
-        img: "img/ship.svg"
+        img: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/ship.svg"
     }, {
         id: 3,
         question: `¿Cuál es la distancia más larga en bicicleta hacia atrás?`,
@@ -24,7 +24,7 @@ let ALLQUESTIONS = [
             "89.30 km", "675.10 km", "337.60 km"
         ],
         correct: "337.60 km",
-        img: "img/bycicle.svg"
+        img: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/bycicle.svg"
     }, {
         id: 4,
         question: `¿Cuál es la velocidad más alta alcanzada nunca por un autobús escolar?`,
@@ -32,7 +32,7 @@ let ALLQUESTIONS = [
             "590 km/h", "320 km/h", "245 km/h"
         ],
         correct: "590 km/h",
-        img: "img/bus.svg"
+        img: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/bus.svg"
     }, {
         id: 5,
         question: `¿Cuál es el viaje más largo con un tanque de gas?`,
@@ -40,15 +40,17 @@ let ALLQUESTIONS = [
             "2,617 km", "3,568 km", "1,732 km"
         ],
         correct: "2,617 km",
-        img: "img/car.svg"
+        img: "https://ihatetomatoes.net/react-tutorials/abc-quiz/images/car.svg"
     }
 ];
 
 const initialState = {
-    questions : ALLQUESTIONS,
+    questions : AllQuestions,
     selected : -1
 };
 
 const store = createStore(initialState);
 
 export default store;
+
+// https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/truck.svg
