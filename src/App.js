1009af910a6704fa46c8e50e2d1762418c6596bf
luisaGonzales <luisagc711@gmail.com>
2017-10-24 07:34:35 -0500

    Agregando hoja componentes

--- src/App.js
+++ src/App.js
@@ -4,100 +4,7 @@ import {connect} from 'redux-zero/react';
 import plane from "./img/plane.svg";
 import {Grid, Row, Col, ProgressBar} from 'react-bootstrap';
 import {next, prev, saveNext, reset, checkQuestions} from './Actions'
-
-const Head = ({image}) => {
-  return (
-    <Row className='text-center'>
-      <Col mdOffset={3}  md={6} mdOffset={3}>
-        <img src={image} alt="imagen"/>
-      </Col>
-    </Row>
-  );
-}
-
-const Progress = ({totalAnswers, totalQuestions}) => {
-  return (
-    <Row className="estado" id="progreso">
-    <Col md={12} xs={12}>
-      <div>
-        {totalAnswers} de {totalQuestions} preguntas contestadas
-      </div>
-      <ProgressBar now={totalAnswers * 20} />
-    </Col>
-  </Row>
-  );
-}
-
-const Quiz = ({question, options, answered}) => {
-  return (
-    <div>
-      <Row>
-        <Col md={12}>
-          <h1 className="text-center titulo">{question}</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={12}>
-          {options}
-        </Col>      
-      </Row>
-    </div>
-  );
-}
-
-const Socials = () => {
-  return ( 
-    <Row>
-      <Col mdOffset={3} md={6} id='redesSociales'>
-        <span className="fa-stack fa-lg ">
-                <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x twitter"></i>
-                <i className="fa fa-twitter fa-stack-1x"></i>
-          </span>
-          <span className="fa-stack fa-lg ">
-                <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x facebook"></i>
-                <i className="fa fa-facebook fa-stack-1x"></i>
-          </span>
-          <span className="fa-stack fa-lg ">
-                <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x google"></i>
-                <i className="fa fa-google-plus fa-stack-1x"></i>
-          </span>
-      </Col>
-    </Row>
-  );
-}
-
-const Navs = ({answers, actualQuestion }) => {
-  return (
-    <div id="flechas" className="text-center">
-      <button id="anterior" className={{answers} >=  {actualQuestion} && {actualQuestion}?'btn':"btn disabled"} onClick={prev}>
-            <img className="img-responsive" src='https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-left-arrow.svg' />
-      </button>
-      <button id="siguiente" className={{answers} >  {actualQuestion} ? 'btn':"btn disabled"} onClick={next}>
-            <img className="img-responsive" src="https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-right-arrow.svg" />
-      </button>
-    </div>
-
-  ) ;
-
-}
-
-const Resume = ({check, correct, questions, userAnswers}) => {
-  <Row>
-    <Col md={12} className='pregunta'>
-      <h1 className="titulo">
-        {!check && 'Estas son tus respuestas'}
-        {check && correct + 'de ' + questions.length + 'correctas!!'}
-      </h1>
-      <div>
-        <span>{userAnswers}</span>
-      </div>
-      <div className='text-center'>
-        {!check && <button className='btn btn-default btn-lg' onClick={reset}>Start Again</button>} }
-        {check && <button className='btn btn-default btn-lg' onClick={checkQuestions}>Enviar</button>}
-      </div>
-    </Col>
-  </Row>
-}
+import {Head, Progress, Quiz, Socials, Navs, Resume} from './Component'
 
 const App = ({questions, answers, actualQuestion, correct, check, answered}) => {
   
@@ -146,7 +53,6 @@ const App = ({questions, answers, actualQuestion, correct, check, answered}) => 
       {!check && questions.length != 0 && 
         <Navs answers={answers.length} actualQuestion={actualQuestion} />
       }
-     
     </Grid>
   );
 }
---
+++ src/Component.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import {Grid, Row, Col, ProgressBar} from 'react-bootstrap';
+import {next, prev, saveNext, reset, checkQuestions} from './Actions'
+
+export const Head = ({image}) => {
+    return (
+      <Row className='text-center'>
+        <Col mdOffset={3}  md={6} mdOffset={3}>
+          <img src={image} alt="imagen"/>
+        </Col>
+      </Row>
+    );
+}
+
+export const Progress = ({totalAnswers, totalQuestions}) => {
+    return (
+      <Row className="estado" id="progreso">
+      <Col md={12} xs={12}>
+        <div>
+          {totalAnswers} de {totalQuestions} preguntas contestadas
+        </div>
+        <ProgressBar now={totalAnswers * 20} />
+      </Col>
+    </Row>
+    );
+}
+
+export const Quiz = ({question, options, answered}) => {
+    return (
+      <div>
+        <Row>
+          <Col md={12}>
+            <h1 className="text-center titulo">{question}</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            {options}
+          </Col>      
+        </Row>
+      </div>
+    );
+}
+
+export const Socials = () => {
+    return ( 
+      <Row>
+        <Col mdOffset={3} md={6} id='redesSociales'>
+          <span className="fa-stack fa-lg ">
+                  <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x twitter"></i>
+                  <i className="fa fa-twitter fa-stack-1x"></i>
+            </span>
+            <span className="fa-stack fa-lg ">
+                  <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x facebook"></i>
+                  <i className="fa fa-facebook fa-stack-1x"></i>
+            </span>
+            <span className="fa-stack fa-lg ">
+                  <i className="fa fa-circle fa-stack-2x fa-inverse fa-2x google"></i>
+                  <i className="fa fa-google-plus fa-stack-1x"></i>
+            </span>
+        </Col>
+      </Row>
+    );
+  }
+
+  export const Navs = ({answers, actualQuestion }) => {
+    return (
+      <div id="flechas" className="text-center">
+        <button id="anterior" className={{answers} >=  {actualQuestion} && {actualQuestion}?'btn':"btn disabled"} onClick={prev}>
+              <img className="img-responsive" src='https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-left-arrow.svg' />
+        </button>
+        <button id="siguiente" className={{answers} >  {actualQuestion} ? 'btn':"btn disabled"} onClick={next}>
+              <img className="img-responsive" src="https://ihatetomatoes.net/react-tutorials/abc-quiz/fonts/navigation-right-arrow.svg" />
+        </button>
+      </div>
+  
+    ) ;
+  
+  }
+
+  export const Resume = ({check, correct, questions, userAnswers}) => {
+    <Row>
+      <Col md={12} className='pregunta'>
+        <h1 className="titulo">
+          {!check && 'Estas son tus respuestas'}
+          {check && correct + 'de ' + questions.length + 'correctas!!'}
+        </h1>
+        <div>
+          <span>{userAnswers}</span>
+        </div>
+        <div className='text-center'>
+          {!check && <button className='btn btn-default btn-lg' onClick={reset}>Start Again</button>} }
+          {check && <button className='btn btn-default btn-lg' onClick={checkQuestions}>Enviar</button>}
+        </div>
+      </Col>
+    </Row>
+  }
